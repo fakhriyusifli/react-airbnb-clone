a198b2e78c01b3585a6f6b6bb0e513f388a8b224
Fakhri Yusifli <55127240+fakhriyusifli@users.noreply.github.com>
2021-05-02 20:09:53 +0400

    added routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
