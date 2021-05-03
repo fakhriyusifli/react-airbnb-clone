77dcc87a957d587c0686d32fd5461e42d942909f
Fakhri Yusifli <55127240+fakhriyusifli@users.noreply.github.com>
2021-05-03 19:59:19 +0400

    added searchPage component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home.jsx";
+import SearchPage from "./components/SearchPage/SearchPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
